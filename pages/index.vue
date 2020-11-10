db2535412e041d468f67297054ccbef693c80ad1
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-11 05:42:19 +0900

    Add form要素

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,33 @@
 <template>
-  <div class="container">テスト表示</div>
+  <div class="container">
+    <v-card>
+      <v-card-title>
+        <h1 class="text-center font-weight-bold">login</h1>
+      </v-card-title>
+    </v-card>
+    <v-form>
+      <v-card-text>
+        <v-text-field
+          label="name"
+          hide-details="auto"
+          type="text"
+        ></v-text-field>
+        <span class="text-caption">エラーメッセージ</span>
+        <v-text-field
+          label="e-mail"
+          hide-details="auto"
+          type="email"
+        ></v-text-field>
+        <v-text-field label="tel" hide-details="auto" type="tel"></v-text-field>
+        <v-text-field
+          label="comment"
+          hide-details="auto"
+          type="textarea"
+        ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary">SEND </v-btn>
+      </v-card-actions>
+    </v-form>
+  </div>
 </template>
