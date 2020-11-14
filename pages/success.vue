04e0a3788c1b2a9d4776855877d79f25bd55c961
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-14 13:44:55 +0900

    Add 送信完了画面表示

--- pages/success.vue
+++ pages/success.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="container">
-    <p>{{ name }}</p>
+    <p>名前：{{ user.name }}</p>
+    <p>email：{{ user.email }}</p>
+    <p>電話番号：{{ user.tel }}</p>
+    <p>コメント：{{ user.comment }}</p>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-  props: ['name'],
+  computed: {
+    ...mapState(['user']),
+  },
 }
 </script>
